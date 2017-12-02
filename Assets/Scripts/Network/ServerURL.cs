1b5bb4d32a773a092f4280e11ccb571b32109df1
김우현 <kkimwooo@WOOMAC.local>
2017-12-02 12:05:48 +0900

    URL CLASS CREATE

---
+++ Assets/Scripts/Network/ServerURL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+	public class ServerURL
+	{
+		public const string ACCOUNT_LOGIN = "/accountLogin.do";
+		public const string GET_COMPANY_INFO= "/GetCompanyInfo.do";
+		public const string ACCOUNT_CHECK = "/AccountCheck.do";
+		public const string REGISTER_ACCOUNT= "/registerAccount.do";
+		public const string ACCOUNT_UPDATE = "/updateAccount.do";
+		public const string ACCOUNT_AUTH= "/accountAuth.do";
+		public const string SEARCH_ACCOUNT_BY_EMAIL = "/searchAccountByEmail.do";
+		public const string GET_DATE = "/getDate.do";
+		public const string GET_STAGE_LIST = "/getStageList.do";
+		public const string GET_USER_INFO = "/getUserInfo.do";
+		public const string SAVE_USER_INFO= "/saveUserInfo.do";
+		public const string GET_USER_LIST= "/getUserList.do";
+		public const string LOGIN_USER = "/loginUser.do";
+
+	}
+
+
